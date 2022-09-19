56ed14ec3e0e6afb7c0f585d1a05635ab4dba943
PashaBk <111427963+PashaBk@users.noreply.github.com>
2022-09-19 11:49:50 +1000

    Add files via upload

---
+++ Урок 7 домашнее задание.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  desription TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products VALUES
+  (DEFAULT, 'Intel 8080', '', 8, 1, DEFAULT, DEFAULT),
+  (DEFAULT, 'Intel 8086', '', 9, 1, DEFAULT, DEFAULT),
+  (DEFAULT, 'MSI 123', '', 34, 2, DEFAULT, DEFAULT);
+
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO catalogs VALUES
+  (DEFAULT, 'Processors'),
+  (DEFAULT, 'Mother boards'),
+  (DEFAULT, 'Video cards');
+
+SELECT p.name, c.name
+FROM products AS p
+INNER JOIN catalogs AS c ON (p.catalog_id = c.id)
+GROUP BY p.id
